88180bfdcb4de83da10e9d9532c6a8ca5c67da5f
Ramil Karimov <ramil.karimov@ikitlab.com>
2020-12-28 11:10:40 +0400

    elder.js start version

---
+++ src/layouts/Layout.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let templateHtml, settings;
+</script>
+
+<style>
+  :global(h1) {
+    font-style: italic;
+  }
+  .container {
+    max-width: 900px;
+    margin: 0 auto;
+    padding: 1rem;
+  }
+
+  :root {
+    --balloon-color: #06395a;
+    --balloon-font-size: 14px;
+  }
+</style>
+
+<svelte:head>
+  <link rel="stylesheet" href="/style.css" />
+</svelte:head>
+
+{@html templateHtml}
